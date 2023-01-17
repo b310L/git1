2185869c952dda0685b89652d2e18a8b3f4e2103
Mhossein <mmhosein.hp@gmail.com>
2023-01-17 16:29:19 +0330

    app js edit=> remove inp.value

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 
-const inp =document.querySelector('#inp');
+// in the name of Allah
 const  handleSubmit = (event) => {
-console.log(inp.value);
-  const p=inp.value;
+console.log('ali');
+  const p='ali';
   alert('A form was submitted: ali ' );
   axios.post('http://localhost:5000/submit',{
   name:p
